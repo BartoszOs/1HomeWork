7f6875e293d40b33db475dfd3e4ad063e46bfa93
teder <t.der@wp.pl>
2018-12-12 01:06:24 +0100

    this time I push whole changes in just one commit

--- Ex1/Ex1App/Program.cs
+++ Ex1/Ex1App/Program.cs
@@ -4,13 +4,14 @@ namespace Ex1App
 {
     class Program
     {
+
         static void Main(string[] args)
         {
             /*
              * Zad 1. Uzupełnij kod tak aby działał wg poniższych wskazówek:
              * Punkty: 3
              * 1. Metoda TellCentury powinna zwracać słownie w którym wieku znajduje się rok podany w parametrze `year`.
-             *    Użyj instrukcji if/else oraz operatorów porównania (<, >, <=, >=). 
+             *    Użyj instrukcji if/else oraz operatorów porównania (<, >, <=, >=).
              *    Obsłuż zakres lat 1500 - 2100
              *    Jeśli podany rok będzie poza tym zakresem - zwróć "Błąd! Podany rok nie jest obsługiwany"
              *
@@ -19,30 +20,110 @@ namespace Ex1App
              *    Pierwszy parametr to rok, 2gi to wiek.
              */
 
-            var year = 2000;
+            Console.WriteLine(" Witaj. Wybierz jedną z opcji : ");
+            Console.WriteLine();
+            Console.WriteLine(" 1 - Tell Century ");
+            Console.WriteLine(" 2 - Tell How Many Years To Century ");
+            Console.WriteLine();
 
-            // wynik dla tego przykładu: "Dwudziesty"
-            TellCentury(year);
+            ConsoleKeyInfo choice = Console.ReadKey();
 
-            // wynik dla tego przykładu: 1
-            HowManyYearsToCentury(1999, 20);
+            switch (choice.KeyChar)
+            {
+                case '1':
+                    Console.WriteLine(" You have chosen option 1 : Tell Century");
+                    Console.WriteLine();
+                    Console.WriteLine(" Enter year from range : 1500 - 2100");
+                    Console.WriteLine();
 
-            // Aby sprawdzić jakiś wynik - wypisz go na ekranie, o tak
-            var result = "Dwudziesty";
-            Console.WriteLine(result);
+                    int year = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine(TellCentury(year));
+                    Console.ReadKey();
+                    break;
+                case '2':
+                    Console.WriteLine(" You have chosen option 2 : Tell How Many Years To Century");
+                    Console.WriteLine();
+                    Console.WriteLine(" Enter a year and then century to calculate how many years are still left to full century");
+                    int givenYear = int.Parse(Console.ReadLine());
+                    int givenCentury = int.Parse(Console.ReadLine());
+                    var result = TellHowManyYearsToCentury(givenYear, givenCentury);
+                    Console.WriteLine($"It is still {result} years left till given {givenCentury} century");
 
-            Console.WriteLine("Press any key to exit...");
-            Console.ReadLine();
+                    TellHowManyYearsToCentury(1999, 20);
+                    Console.ReadKey();
+                    break;
+                default:
+                    Console.WriteLine(" Choice is out of range. Exiting ...");
+                    Console.ReadKey();
+                    break;
+            }
+
         }
 
-        private static int HowManyYearsToCentury(int year, int century)
-        {
-            return 1;
-        }
+        //     * 1. Metoda TellCentury powinna zwracać słownie w którym wieku znajduje się rok podany w parametrze `year`.
+        //     *    Użyj instrukcji if/else oraz operatorów porównania(<, >, <=, >=).
+        //     *    Obsłuż zakres lat 1500 - 2100
+        //     *    Jeśli podany rok będzie poza tym zakresem - zwróć "Błąd! Podany rok nie jest obsługiwany"
 
         private static string TellCentury(int year)
         {
-            return "foo";
+            string result = "";
+
+            if ((year >= 1500) && (year <= 2100))
+            {
+                if (year == 1500)
+                {
+                    result = "Piętnasty";
+                }
+
+                else if ((year > 1500) && (year <= 1600))
+                {
+                    result = "Szesnasty";
+                }
+
+                else if ((year > 1600) && (year <= 1700))
+                {
+                    result = "Siedemnasty";
+                }
+
+                else if ((year > 1700) && (year <= 1800))
+                {
+                    result = "Osiemnasty";
+                }
+
+                else if ((year > 1800) && (year <= 1900))
+                {
+                    result = "Dziewiętnasty";
+                }
+
+                else if ((year > 1900) && (year <= 2000))
+                {
+                    result = "Dzwudziesty";
+                }
+
+                else
+                {
+                    result = "Dzwudziesty pierwszy";
+                }
+            }
+            else
+            {
+                result = "Błąd! Podany rok nie jest obsługiwany";
+            }
+
+            return result;
+        }
+
+        /* 2. Metoda TellHowManyYearsToCentury powinna zwracać liczbę lat do osiągnięcia pełnego wieku od wskazanego roku.
+         *    Pierwszy parametr to rok, 2gi to wiek.
+         */
+
+        private static int TellHowManyYearsToCentury(int year, int century)
+        {
+            string centuryAsString = century.ToString();
+            centuryAsString += "00";
+            int centToYears = int.Parse(centuryAsString);
+            return centToYears - year;
         }
     }
 }
